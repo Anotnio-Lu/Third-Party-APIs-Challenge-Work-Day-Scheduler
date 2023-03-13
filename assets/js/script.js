bd03afc81c708f921d424f93af32d3aabf92bbc6
Antonio Lu <Antonioraylu@outlook.com>
2023-03-14 01:31:42 +1100

    JS update: on click listener added to save input into local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var list = []
+
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +22,45 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
+
+  var main = $('.main-container')
+  var saveBtn = $('.saveBtn')
+
+
+  main.on('click', '.fas', function(event){
+
+    var item = $(this)
+    var parent = item.parent().parent('div')
+    var text = parent.children('textarea').val()
+    var dataId = parent.attr("id");
+
+    let storedInfo = JSON.parse(localStorage.getItem("To do list"));
+
+    if(storedInfo !== null){
+        list = storedInfo
+    } 
+
+    $.each ( list, function( indexes, values ) {
+      if (dataId == values.id){
+        list.splice(indexes, 1);
+        return false
+      }
+    })
+
+    let timeInto = new listObject();
+    timeInto.id = dataId
+    timeInto.input = text
+
+    list.push(timeInto);
+
+    localStorage.setItem("to do list", JSON.stringify(list))
+
+  })
+
+  function listObject(){
+    this.id
+    this.input
+  }
+
+
+});
